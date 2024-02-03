a3455954165ccf7117b245062920be393f667e9c
Micky <micky.mari@etu.u-bordeaux.fr>
2024-02-03 12:59:04 +0100

    New formatage de la base de donnée
    Ici je vais tout changer sur le syst JS donc, je dois commencer par la base de donnée que je vais utiliser

---
+++ base_donnée.js
@@ -0,0 +1,206 @@
+// CECI EST LE FICHIER BASE DE DONNEE
+
+donnees = [
+`Titre : Batard
+Genre : Thriller
+Statut : Terminé
+Nombre de chapitres : 93
+Taille d'un chapitre : Court 
+Description courte : Nous allons suivre le quotidien d'un fils ainsi que de son père, qui n'ont pas un quotidien des plus classiques. Pourquoi le père tue-t-il autrui ? Que s'est-il passé avec la mère ? Quels sont les anciens souvenirs perdus de l'enfant ?
+Toutes ces questions font partie de l'intrigue principale de ce Webtoon horreur.
+Avis Personnel : Je n'ai pas beaucoup vu de Thriller mais celui ci m'a beaucoup plu, on suit l'évolution du personnage principal avec grand intérêt, les moments de frissons sont bien rythmé, on compatis pour le personnage et comprends en même temps que lui ce qui lui arrive.
+Lien : https://www.webtoons.com/fr/thriller/bastard/list?title_no=1830
+Dernière date d'édition : 9/11/23
+`,
+
+`Titre : Capitaine Zorgue
+Genre : Comedy / Slice of Life
+Statut : Terminé 
+Nombre de chapitres : 237
+Taille d'un chapitre : Court
+Description : La Terre a construit un vaisseau ultra performant pour se sauver de notre planète qui se meurt peu à peu. Elle a donc choisi les meilleurs pour partir dans une mission sans retour afin de trouver une nouvelle planète habitable. Les meilleurs ayant refusé, on a fait un équipage avec ceux qui restaient. Le Capitaine Zorgue est le capitaine de ce super vaisseau et de son tout aussi superbe équipage.
+Avis Personnel : Une très bonne comédie, le personnage est attachant, certains personnages servent de running gag bien utilisé. Les graphismes sont simples et l'histoire drôle.
+Lien : https://www.webtoons.com/fr/sf/capitaine-zorgue/list?title_no=2370
+Dernière date d'édition : 9/11/23
+`,
+
+`Titre : L'expert de la Tour Tutoriel
+Genre : Fantastique / Action
+Statut : En cours
+Nombre de chapitres : 162
+Taille d'un chapitre : Moyen
+Description : Après avoir été enfermé pendant 12 ans dans la tour tutoriel, le monde me considérait comme un expert. Maintenant que je suis sorti, ma vraie aventure commence !
+Avis Personnel : Une histoire assez bannale avec un principe de tours à monter, récurrent dans pas mal de webtoon, selon moi ce webtoon est bien sans plus, si vous n'accrochez de base que moyennement à ce type de webtoon, il y a mieux, plus diversifié. Celui ci à son système mais ne sort pas vraiment de l'ordinaire. Malgré tout, si vous cherchez juste à lire quelque chose pour passer le temps il s'agit alors d'un très bon webtoon.
+Avis de Vin's : Je le lisais, au début il y avait pas mal d'humour mais plus on avance plus on s'éloigne du personnage de base et plus les "blagues" tourne en rond, donc j'ai laissé tombé autour de l'épisode 120.
+Lien : https://www.webtoons.com/fr/fantasy/expert-de-la-tour-tutoriel/list?title_no=2312
+Dernière date d'édition : 13/11/23
+`,
+
+`Titre : Prodige Hors Norme
+Genre : Fantastique / Action / Comédie
+Statut : En cours
+Nombre de chapitres : 16
+Taille d'un chapitre : Moyen
+Description : Kwang-ik est un jeune de 20 ans, et il apprend un jour qu'il est métis de deux races spéciales, un cas peu commun. Ses parents, qui avaient tous les deux caché leur race l'un de l'autre, commencent à préparer leur fils avec des entraînements typiques à leur race. Avec ces entraînements, Kwang-ik apprend qu'il possède un mélange de pouvoirs des deux races, ce qui le rend plus puissant qu'un Immortel ou un Métamorphe de sang pur. Une fois les entraînements de base terminés, il décide de joindre une entreprise d'Immortels par la proposition de son père.
+Avis Personnel : Franchement, une bonne série pour se détendre, l'humour est présente à foison, l'action aussi. Le mélange des 2 races nous donne des quiproquos drôle mais facilite aussi la vie du personnage principal qui lui aime bien embêter autrui. Il est impulsif mais drôle et attachant.
+Lien : https://www.webtoons.com/fr/fantasy/prodige-hors-norme/list?title_no=5582
+Dernière date d'édition : 9/11/23
+`,
+
+`Titre : La mariée du roi dragon
+Genre : Romance ; Fantasy ; Drama
+Statut : Pas terminer, 31 épisode
+Taille d'un chapitre : Mi-long Mi-court cela varie
+Description : 
+Nous allons suivre l'histoire d'une jeune femme au nom de Lucina, née fille illégitime d'un baron, ça famille ainsi que les domestiques de la maison lui mènera la vie dure, sa famille l'a également promise au roi du pays étant connu pour tuer ces conquêtes lorsque celui-ci se lasse d'elle. Mais un beau jour, en crainte de leurs vie, la famille de Lucina la vendu au dénommé roi des barbares qui celui-ci l'enleva enfin de faire d'elle sa femme.
+Avis personnelle :
+Perso je trouve que l'histoire entre les 2 protagoniste va assez vite et que sa perd son intérêt petit a petit (et j'avais aussi envie de taper la meuf a pls reprise mais sa c autre chose mdr) mais sinon en soit malgré la rapidité de l'histoire et le fait ue du coup sa colle pas trop avec le perso princ, j'aime quand même bien l'hisoire et en plus je trouve que les dessins sont très beau aussi!
+Liens :
+https://www.webtoons.com/fr/romance/la-mariee-du-roi-dragon/list?title_no=5339
+Dernière date d'édition : 8/11/23
+`,
+
+`
+Titre : Like Wind on a Dry Branch
+Genre : Fantasy ; Romance 
+Statut : Pas terminer, 140 épisodes
+Taille des chapitres : mi court mi long, cela varie
+Description : 
+Rietta Tristi est une jeune femme vivant dans une époque où les "démons", plus précisément la peste, fait rage. La maladie ayant emporter son mari, elle se retrouve face à la menace d'un noble malveillants, voulant faire d'elle ça femme, celui-ci fit enlever sa fille afin de la vendre en esclavage suite à son refus. Avant sa mort, il demanda à ce que Rietta soit enterrer vivante à ces côtés. Cependant la vie de la jeune femme se voit sauver par la visite surprise d'un prince déchu. 
+Avis personnelle :
+Perso j'aime bien le webtoon, il y a du drama mais c'est un webtoon qui reste calme, doux et apaisant, que ce soit au niveau de l'histoire, qu'au niveau des dessins. Je trouve même que ce webtoon reste l'un, voir le plus beau webtoon que j'ai lu jusqu'ici. Je conseille de le lire si vous cherchez un peu de douceur mdr
+Liens :https://www.webtoons.com/fr/fantasy/like-wind-on-a-dry-branch/list?title_no=2572
+Dernière date d'édition : 8/11/23
+`,
+
+`
+Titre : For my derelict favorite
+Genre : Romance ; comique
+Statut : En pause (s1 terminer) 
+Nombre de chapitres : 55 + 1
+Taille d'un chapitre : Moyen
+Description :
+Nous savons souvent comment se déroule une histoire dans un livre, mais savons-nous que ce passe t'il après le "heureux jusqu'à la fin des temps"? 
+C'est une réponse que nous aurons en suivant l'histoire Hestia. 
+Hestia est une jeune femme ayant était assassinée dans son ancienne vie, cependant la vie lui octrois une réincarnation qui lui plaît fortement, étant une grande fan girl de roman, quel à était sa surprise en se réveillant dans l'univers de son roman préféré! Fan girl comme elle est, elle laissera étonnamment l'histoire se dérouler comme tel, essayant juste de voir de près son character préféré! Avant de découvrir la fin tragique de celui-ci! 
+Sous un coup de rage et de tristesse, elle perdit la vie dans un dernier instant de prière, avant de se réveiller une dernière fois, à la dernière journée, de son roman préféré! 
+Avis Personnel : 
+Sa semble un peu répétitif mais je le rassure sa l'est pas, la 1er réincarnation est très courte pour laisser plus de place à la 2eme qui est vachement plus importante, ce que j'aime bien dans ce webtoon c le fait que la fan girl me fait bcp rire perso, j'aime bcp l'histoire qu'est créer dans cette univers et j'aime aussi bcp le fait que pour une fois les méchants sont les personnages principaux (pas les 2 perso prin du webtoon mais les 2 protagonistes de histoire dans laquelle hestia c réincarné) et j'adore aussi bcp qu'elle les mène vrmt en bourrique. J'aime aussi bcp les dessins mais sa c autre chose encore mdr
+Lien : https://www.webtoons.com/fr/romance/for-my-derelict-favorite/list?title_no=4957
+Dernière date d'édition : 10/11/23
+`,
+
+`
+Titre : Lecteur omniscient
+Genre : action/fantastique
+Statut : en cours
+Nombre de chapitres : 180(variable selon la langue et la plateforme)
+Taille des chapitres : Long
+Description : alors qu'il n'est qu'un étudiant ordinaire, Kim Dojka fini le dernier chapitre de "Trois façon de survivre dans un monde en ruine", dont il est l'unique lecteur.
+La terre est alors frappée de catastrophes, et les êtres humains sont transformés en divertissement pour les "constellation", des êtres ayant obtenu il y a longtemps une grande reconnaissance par les hommes (Dieu, héro, personnage célèbre, etc.).
+Les personnes les plus chanceuses sont reconnues comme "incarnation " d'une constellation, et obtiennent des pouvoirs de celle-ci.
+Notre héros reconnaît aussitôt l'histoire qu'il vient de finir... Et il fera tout pour y survivre !
+Avis personnel: un début très classique et certains personnages clichés, mais au fur et à mesure le style se développent et même si certains événements récurrents surviennent, l'histoire est intéressante et parfois très drôle.
+Par contre les personnages n'évoluent pas beaucoup à mon goût...
+https://www.webtoons.com/fr/fantasy/omniscient-reader/ep177/viewer?title_no=2175&episode_no=178
+`,
+
+`
+Titre : Solo Auto-Hunting
+Genre : Action
+Statut : Terminé
+Nombre de chapitres : 92
+Taille d'un chapitre : Moyen
+Description : Oh Yuseong travaille dans une supérette et vient de se faire larguer par sa copine qui ne le trouve pas assez ambitieux. Alors qu’il touche le fond, une fissure devant lui libère un monstre qui s'en prend à lui. Aux portes de la mort, le mystérieux pouvoir de la "chasse en mode automatique" s’éveille en lui. Maintenant il n’a plus qu’à le maîtriser pour devenir le meilleur des chasseurs !
+Avis Personnel : Pas encore écris
+Avis de Vin's : Alors clairement (si je puis me permettre) la fin est juste ... Nulle
+Je le lisais quand il sortait et au bon d'un moment il arrive un arc qui vient casser le rythme du webtoons, en diminuant énormément les combats, leur intérêt et autres, et qui se concentre sur des manœuvres presque politique mais vraiment mal faites, et des explications pas convaincante, quand à la fin... La série s'est arrêtée d'un coup, la fin n'en est pas une.... Bref très bon début et une fin très décevante
+Lien : https://www.webtoons.com/fr/action/solo-auto-hunting/list?title_no=3488
+Dernière date d'édition : 13/11/23
+`,
+
+`
+Titre : The World After The Fall
+Genre : Action
+Statut : En cours
+Nombre de chapitres : 85 chap
+Taille d'un chapitre : Moyen / Long
+Description : Lorsque l’apocalypse a éclaté, Jaehwan a été convoqué par la « Tour des cauchemars »...
+Avis Personnel : Pas un simple système de tour le plus basique, on voit vraiment la façon de penser du personnage qui évolue...
+Lien : https://www.webtoons.com/fr/action/the-world-after-the-fall/list?title_no=4193
+Dernière date d'édition : 9/11/23
+`,
+
+`
+Titre : Hero Ticket
+Genre : Action / Slice of Life
+Statut : En cours
+Nombre de chapitres : 189
+Taille d'un chapitre : Je sais plus (si quelqu'un regarde il répond à mon commentaire et j'édite le post)
+Description : L'obstination à mourir de Kyeong-Ho séduit Sierra, une faucheuse des enfers, qui lui propose un pacte. Ce pacte lui donne la possibilité d'emprunter les pouvoirs des morts en échange d'années de vie. Afin d'obtenir ce qu'il désire, il finit par l'accepter.
+Avis Personnel : De la romance est présente, pas dérangeante pour autant, l'histoire oscille entre les intérêts du personnage et ce qui ce passe dans son entourage, le personnage principal est à la fois réfléchis et plutôt tête brulé, il comprend au fil du temps de nouvelle façon d'utiliser son pouvoir, ici l'originalité viens aussi du fait qu'on oublie au fil de l'histoire le temps de vie qu'il le lui reste à cause de cette échange justement.
+Lien : https://www.webtoons.com/fr/action/heroticket/list?title_no=2178
+Dernière date d'édition :  9/11/23
+`,
+
+`
+Titre : Je ne suis pas celui que vous croyez
+Genre : Action / Slice of Life / Comedie
+Statut : En cours
+Nombre de chapitres : 36
+Taille d'un chapitre : Moyen
+Description : Demon Arut est le capitaine de la Légion zéro, l'un des treize ordres au service du roi des démons. Bien qu'il soit humain, son seul nom suffit à faire trembler d'effroi ceux qui l'entendent, car il est cruel et impitoyable. Enfin ça, c'est la version officielle. En réalité, il s'appelle Deon Hart, manque de s'évanouir à la moindre anxiété et s'est retrouvé à la tête de la Légion zéro sur un malentendu. Il n'aspire donc qu'à une chose : s'enfuir du royaume des démons avant que le pot aux roses ne soit découvert. Une tâche cependant bien ardue lorsque l'on est le prodige adoré du souverain démoniaque...
+Avis Personnel : Une histoire qui me plaît, on a les 2 côtés, que ce soit le côté humain ou démon, tous se méprennent sur son sujet, lui ne comprends pas comment il en est arrivé là et ce retrouve toujours dans des situations complexes. La vengeance envers son Frère est un points des plus important de l'histoire, il fait avancer et ce surmener le personnage principal. Malgré la présence de démon et d'humain, il ne s'agit pas d'une fantasy avec de la magie de partout et des techniques d'épées très développé à tout vas, il s'agit plutôt de montrer la faiblesse du personnage principal qui lui crache du sang pas la simple action de se tenir debout. En somme une histoire intéressante, alliant comédie, action et tranche de vie.
+Lien : https://www.webtoons.com/fr/action/je-ne-suis-pas-celui-que-vous-croyez/list?title_no=5276
+Dernière date d'édition : 9/11/23
+`,
+
+`
+Titre : The Lazy Lord Masters the Sword
+Genre : Action / Slice of Life / Fantastique
+Statut : S1 terminée, en attante de la saison 2
+Nombre de chapitres : 86
+Taille d'un chapitre : Moyen / Long
+Description : Après avoir assisté impuissant à la mort de sa mère, le jeune Airn Pareira profite du sommeil pour échapper à la dure réalité, ce qui lui vaut le surnom de « lord paresseux ». Pourtant, malgré ce que les autres disent de lui, Airn n'a pas l'intention de changer ses habitudes. Mais la vie de ce dernier va prendre un tout autre tournant lorsqu’un jour il se réveille d'un rêve sur un mystérieux épéiste.
+Avis Personnel : Le Webtoon figurant à la seconde place dans la liste de mes préférés, il utilise des choses plutôt basique dans un univers classique, mais la façon dont le personnage progresse, se lie aux autres, les personnages secondaires qui sont bien écrits. La progression lente mais entièrement sous contrôle et qui donc donne de l'adrénaline quand on se rend compte de la puissance du personnage. Un très bon Webtoon qui ne rush pas la progression.
+Lien : https://www.webtoons.com/fr/action/the-lazy-lord-masters-the-sword/list?title_no=3675
+Dernière date d'édition : 9/11/23
+`,
+
+`
+Titre : Return of the Mad Demon
+Genre : Action
+Statut : En cours 
+Nombre de chapitres : 74
+Taille d'un chapitre : Moyen
+Description : Lee Jaha, surnommé le démon fou par ses ennemis, est revenu dans le passé après qu'une seconde chance lui a été octroyée. Cette fois-ci, sa vie ne va pas être réduite à néant. Il fera tout ce qui sera en son pouvoir pour protéger les opprimés et écraser leurs bourreaux. Mais sera-t-il à la hauteur pour vaincre ses ennemis dans sa deuxième vie ?
+Avis Personnel : Un Webtoon sur un univers de type Murim, mais le personnage principal est vraiment fou, il est parfois sains mais certaines fois s'adonne à des tueries. C'est à la fois la beauté du personnage et la chose traumatisante qui le caractérise, il s'en prend uniquement au personnes mauvaises comme les violeurs, vendeurs d'esclaves. C'est l'histoire sur le Murim la plus violente que je connais, il est à mon avis plutôt gore pour un Webtoon (même si sa va).
+Lien : https://www.webtoons.com/fr/action/return-of-the-mad-demon/list?title_no=4456
+Dernière date d'édition : 9/11/23
+`,
+
+`
+Titre : Auto Hunting With My Clones
+Genre : Action
+Statut : En cours 
+Nombre de chapitres : 53
+Taille d'un chapitre : Moyen
+Description : Travailler, étudier, manger… Tant de choses à faire et si peu de temps pour le reste ! Si seulement on pouvait se dédoubler, la vie serait tellement plus simple ! Le vœu de Sangwoo se réalise le jour de son "Éveil” : il a désormais le pouvoir de se démultiplier. Il décide d'exploiter sa nouvelle compétence pour devenir un chasseur : il sera le cerveau et ses clones, ses marionnettes.
+Avis Personnel : Un Webtoon sur un univers de type monde avec des rank et de la magie. Notre personnage lui ce différencie par son pouvoir de clonage, lui possède des clones tangible ce qui lui permet de pouvoir travailler ses caractéristiques et compétences plus rapidement. Le webtoon n'est pas redondant par rapport à d'autres. Notre personnage évolue tranquillement à son rythme avec plusieurs passage que l'on peut reconnaître. Le webtoon n'en est encore qu'à 53 chapitres, j'attend une continuation et réussite de l'histoire en espérant que l'histoire ne devienne pas un cliché sur la fin.
+Avis de Vin's : Un webtoon sympa, un peu cliché mais très drôle à lire, j'approuve
+Lien : https://www.webtoons.com/fr/action/auto-hunting-with-my-clones/list?title_no=5031
+Dernière date d'édition : 13/11/23
+`,
+
+`
+Titre : Villain To Kill
+Genre : Action
+Statut : En Cours
+Nombre de chapitres : 112
+Taille d'un chapitre : Moyen
+Description : Cassian est un héros légendaire, un « psyker » de première classe qui a perdu la vie, injustement accusé du meurtre de son collègue. À son réveil, il se retrouve dans le corps d’un jeune élève et découvre qu’il est devenu un de ces « vilains » qu’il chassait dans sa vie précédente...! Cassian devra désormais compter sur « Moros » une mystérieuse créature, pour combattre le désir destructif qui s’empare constamment de son corps de vilain, afin de pouvoir se venger de ceux qui l’ont trahi dans sa vie passée. « Puisque c'est comme ça, je deviendrai le plus puissant des vilains à ma manière ! »
+Avis Personnel : Ici encore sur un monde avec des ranks, mais finalement assez différent, je propose d'en lire un peu pour comprendre. Mais notre héro n'est pas un gentil, mais pas un méchant. Sa situation ambigüe est sympa à lire et peu de webtoon lui ressemble. Je le recommande (il y a plus de chapitre en anglais).
+Lien : https://www.webtoons.com/fr/action/villaintokill/list?title_no=3443
+Dernière date d'édition : 14/11/23
+`
+]
