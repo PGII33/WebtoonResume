8091707067ba8814a9cbb6300f6c0729f434d929
Micky <micky.mari@etu.u-bordeaux.fr>
2024-02-04 07:29:23 +0100

    updt base_donnee minimal version
    J'ai réduit temporairement la base de donnée pour des tests

---
+++ js/base_donnee.js
@@ -0,0 +1,39 @@
+var webtoons = [
+    {
+        title: "Batard",
+        genre: "thriller",
+        status: "Terminé",
+        chapters: 93,
+        chapterSize: "Court",
+        description: "Nous allons suivre le quotidien d'un fils ainsi que de son père, qui n'ont pas un quotidien des plus classiques. Pourquoi le père tue-t-il autrui ? Que s'est-il passé avec la mère ? Quels sont les anciens souvenirs perdus de l'enfant ? Toutes ces questions font partie de l'intrigue principale de ce Webtoon horreur.",
+        personalReview: "Je n'ai pas beaucoup vu de Thriller mais celui-ci m'a beaucoup plu, on suit l'évolution du personnage principal avec grand intérêt, les moments de frissons sont bien rythmés, on compatis pour le personnage et comprend en même temps que lui ce qui lui arrive.",
+        vinReview: "",  
+        link: "https://www.webtoons.com/fr/thriller/bastard/list?title_no=1830",
+        lastEditDate: "9/11/23"
+    },
+    {
+        title: "Capitaine Zorgue",
+        genre: "Comedy / SoL",
+        status: "Terminé",
+        chapters: 237,
+        chapterSize: "Court",
+        description: "La Terre a construit un vaisseau ultra performant pour se sauver de notre planète qui se meurt peu à peu. Elle a donc choisi les meilleurs pour partir dans une mission sans retour afin de trouver une nouvelle planète habitable. Les meilleurs ayant refusé, on a fait un équipage avec ceux qui restaient. Le Capitaine Zorgue est le capitaine de ce super vaisseau et de son tout aussi superbe équipage.",
+        personalReview: "Une très bonne comédie, le personnage est attachant, certains personnages servent de running gag bien utilisé. Les graphismes sont simples et l'histoire drôle.",
+        vinReview: "",  
+        link: "https://www.webtoons.com/fr/sf/capitaine-zorgue/list?title_no=2370",
+        lastEditDate: "9/11/23"
+    },
+
+    {
+        title: "L'expert de la Tour Tutoriel",
+        genre: "Fantastique / Action",
+        status: "En cours",
+        chapters: 162,
+        chapterSize: "Moyen",
+        description: "Après avoir été enfermé pendant 12 ans dans la tour tutoriel, le monde me considérait comme un expert. Maintenant que je suis sorti, ma vraie aventure commence !",
+        personalReview: "Une histoire assez banale avec un principe de tours à monter, récurrent dans pas mal de webtoons. Selon moi, ce webtoon est bien sans plus. Si vous n'accrochez de base que moyennement à ce type de webtoon, il y a mieux, plus diversifié. Celui-ci a son système mais ne sort pas vraiment de l'ordinaire. Malgré tout, si vous cherchez juste à lire quelque chose pour passer le temps, il s'agit alors d'un très bon webtoon.",
+        vinReview: "Je le lisais, au début il y avait pas mal d'humour, mais plus on avance plus on s'éloigne du personnage de base et plus les 'blagues' tournent en rond. Donc j'ai laissé tombé autour de l'épisode 120.",
+        link: "https://www.webtoons.com/fr/fantasy/expert-de-la-tour-tutoriel/list?title_no=2312",
+        lastEditDate: "13/11/23"
+    },
+];
