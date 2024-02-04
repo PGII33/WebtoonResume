445436664a22ba2fe918648682c179afb065b25c
Micky <micky.mari@etu.u-bordeaux.fr>
2024-02-04 18:35:13 +0100

    updt fonction
    updt :
     - toggleVisibility
     - genereResume
     - filtrerParGenre

--- js/fonctions.js
+++ js/fonctions.js
@@ -4,28 +4,31 @@
 function toggleVisibility(id) {
     var hiddenText = document.getElementById(id);
 
-    if (window.getComputedStyle(hiddenText).display === "none") {
+    if (hiddenText && (hiddenText.style.display === "none" || hiddenText.style.display === "")) {
         hiddenText.style.display = "block";
-    } else {
+    } else if (hiddenText) {
         hiddenText.style.display = "none";
     }
 }
+
+
 
 // Cette fonction permet de séparer du texte brut en catégories et en texte
 function genererResume(webtoons) {
     let html = '<div class="webtoon-container">'; // Ajout d'un conteneur pour les webtoons
 
     for (const webtoon of webtoons) {
+        
         let genre = webtoon.genre.replace(/\//g, '')
         let title_ = webtoon.title.replace(/ /g, '_')
         html += `
         <div class="webtoon">
-            <div class="toggle-text ${genre}" onclick="toggleVisibility('${webtoon.title}')">
+            <div class="toggle-text ${genre}" onclick="toggleVisibility('${title_}')">
                 <strong>${webtoon.title}</strong>
             </div>
             
             <div class="webtoon-info">
-                <p class="hidden-text" id="${webtoon.title_}"></p>
+                <p class="hidden-text" id="${title_}">
                     <strong> Titre </strong> : ${webtoon.title} <br\> <br\>
                     <strong> Genre </strong> : ${webtoon.genre} <br\> <br\>
                     <strong> Statut </strong> : ${webtoon.status} <br\> <br\>
@@ -45,14 +48,13 @@ function genererResume(webtoons) {
     return html;
 }
 
-
 // Cette fonction permet le filtrage par genre
 function filtrerParGenre() {
     const genreSelectionne = document.getElementById('genreFilter').value;
     const elementsWebtoon = document.getElementsByClassName('toggle-text');
 
     for (const element of elementsWebtoon) {
-        if (genreSelectionne === 'tous' || element.classList.contains(genreSelectionne)) {
+        if (genreSelectionne === 'Tous' || element.classList.contains(genreSelectionne)) {
             element.style.display = 'block';
         } else {
             element.style.display = 'none';
